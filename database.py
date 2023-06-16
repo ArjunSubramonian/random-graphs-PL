6ca53e3081e5a760d4f8e564a1ca617faa6e012c
Ana Brendel <anabrendel25@gmail.com>
2023-06-16 00:49:18 -0700

    restructure code to match other formats and include observe/pr

--- database.py
+++ database.py
@@ -1,44 +1,19 @@
-import numpy as np
-from numpy.random import binomial, choice
-import itertools
-import time
-
-import signal
-
-
-class TimeoutException(Exception):  # Custom exception class
-    pass
-
-
-def timeout_handler(signum, frame):  # Custom signal handler
-    raise TimeoutException
-
-
-# Change the behavior of SIGALRM
-signal.signal(signal.SIGALRM, timeout_handler)
-
-import sys
-
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
-
-class RandomGraph:
-    def __init__(self, num_nodes, prob_adj_list, undirected=True):
+class ProbabilisticDatabase:
+    def __init__(self, G):
         # set up the database and class variables
-        self.undirected = undirected
+        self.undirected = G.undirected
         self.db = [(1, [])]
-        self.n = num_nodes
-
+        self.n = G.num_nodes
+        # From graph
         # get all of the possible edges with their associated probability (from adjacency list)
+        db = [(1, [])]
         edges = []
-        for src in prob_adj_list.keys():
-            for tgt, prob in prob_adj_list[src]:
+        for src in G.out_adj_list.keys():
+            for tgt, prob in G.out_adj_list[src]:
                 # if directed, add every edge
-                if not undirected:
+                if not G.undirected:
                     edges.append((src, tgt, prob))
-                elif undirected and (tgt, src, prob) not in edges:
+                elif G.undirected and (tgt, src, prob) not in edges:
                     edges.append((src, tgt, prob))
 
         # populate the database and store the associated probabilties with each world
@@ -50,88 +25,63 @@ class RandomGraph:
                 for src_node, tgt_node in world:
                     new_world.append((src_node, tgt_node))
                 new_world.append((src, tgt))
-                if undirected:
+                if G.undirected:
                     new_world.append((tgt, src))
                 self.db[w] = (((1 - prob) * prob_og), world)
                 self.db.append(((prob * prob_og), new_world))
 
-
-# naive counting --> returns the expected value of the number of triangles
-def count_triangles(db_graph):
-    dist = {}
-    for w in range(len(db_graph.db)):
-        world_tri_total = 0
-        (wp, world) = db_graph.db[w]
-        for i in range(db_graph.n):
-            # if undirected, just needs to check the one direction
-            if db_graph.undirected:
-                for j in range(i, db_graph.n):
-                    if (i, j) in world:
-                        for k in range(j, db_graph.n):
-                            if (i, k) in world and (j, k) in world:
-                                world_tri_total += 1
-        triangle_key = f"{world_tri_total}"
-        if triangle_key in dist.keys():
-            dist[triangle_key] = dist[triangle_key] + wp
-        else:
-            dist[triangle_key] = wp
-    values = []
-    ret_dist = []
-    for key in dist.keys():
-        values.append(int(key))
-        ret_dist.append(dist[key])
-    return values, ret_dist  # returns the expected value of number of triangles
-
-
-########################## Testing Loops (reusing from mc.py) ###########################
-print("[")
-for num_nodes_step in range(4, 10):
-    # num_nodes = 2**num_nodes_step
-    num_nodes = num_nodes_step
-    eprint("num nodes = {}".format(num_nodes))
-
-    times = []
-    for p_step in range(1, 6):
-        p = 0.2 * p_step
-        for q_step in range(1, 6):
-            q = 0.2 * q_step
-            eprint("p = {}".format(p), "q = {}".format(q))
-
-            signal.alarm(60)  # time out after a minute
-            start_time = time.time()
-            try:
-                prob_adj_list = {n: [] for n in range(num_nodes)}
-                for source in range(num_nodes):
-                    for target in range(source + 1, num_nodes):
-                        if source < num_nodes // 2 and target < num_nodes // 2:
-                            prob = p
-                        elif source >= num_nodes // 2 and target >= num_nodes // 2:
-                            prob = p
-                        else:
-                            prob = q
-
-                        prob_adj_list[source].append((target, prob))
-
-                G = RandomGraph(num_nodes, prob_adj_list)
-                values, dist = count_triangles(G)
-                eprint(np.dot(values, dist))
-            except TimeoutException:
-                pass
+    def pr(self, use_cached=False):
+        dist = {}
+        for w in range(len(self.db)):
+            world_tri_total = 0
+            (wp, world) = self.db[w]
+            for i in range(self.n):
+                # if undirected, just needs to check the one direction
+                if self.undirected:
+                    for j in range(i, self.n):
+                        if (i, j) in world:
+                            for k in range(j, self.n):
+                                if (i, k) in world and (j, k) in world:
+                                    world_tri_total += 1
+            triangle_key = f"{world_tri_total}"
+            if triangle_key in dist.keys():
+                dist[triangle_key] = dist[triangle_key] + wp
             else:
-                # Reset the alarm
-                signal.alarm(0)
+                dist[triangle_key] = wp
+        values = []
+        ret_dist = []
+        for key in dist.keys():
+            values.append(int(key))
+            ret_dist.append(dist[key])
+        return dict(zip(values, ret_dist))
+    
+    def observe_edge(self, s, t):
+        normalize = 0
+        updated_db = []
+        for w in range(len(self.db)):
+            (wp, world) = self.db[w]
+            if ((s,t) in world) or ((t,s) in world):
+                normalize += wp
+                updated_db.append((wp,world))
+        self.db = []
+        for (wp,world) in updated_db:
+            self.db.append(((wp/normalize),world))
+    
+    def observe_no_edge(self, s, t):
+        normalize = 0
+        updated_db = []
+        for w in range(len(self.db)):
+            (wp, world) = self.db[w]
+            if ((s,t) not in world) and ((t,s) not in world):
+                normalize += wp
+                updated_db.append((wp,world))
+        self.db = []
+        for (wp,world) in updated_db:
+            self.db.append(((wp/normalize),world))
 
-            time_elapsed = time.time() - start_time
-            times.append(time_elapsed)
-
-    eprint()
-    eprint(
-        "time taken:",
-        str(np.mean(np.array(times))) + " ± " + str(np.std(np.array(times))),
-    )
-    eprint()
-
-    print(
-        f'{{  "n": {num_nodes}, "mean": {str(np.mean(np.array(times)))}, "std": {str(np.std(np.array(times)))}, "times": {times} }},'
-    )
-print("]")
+    def observe_triangle(self, a, b, c):
+        if not self.undirected:
+            return ValueError("Triangle observations are only supported for undirected graphs.")
+        self.observe_edge(a, b)
+        self.observe_edge(b, c)
+        self.observe_edge(a, c)
